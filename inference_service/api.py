1edc8032fbbb360431d6a9a7d1a0320b554c33d2
Adrien Legros <alegros@redhat.com>
2023-09-22 16:04:56 +0200

    cvalue

--- inference_service/api.py
+++ inference_service/api.py
@@ -59,7 +59,7 @@ async def predictions(file: UploadFile = File(...)):
         print(class_number)
         print(classes)
         detection = Detection(box=box, 
-            cValue=None, class_=classes[class_number], label=classes[class_number].capitalize(), score=raw_detection[4])
+            cValue=5, class_=classes[class_number], label=classes[class_number].capitalize(), score=raw_detection[4])
         print(detection)
         result.detections.append(detection)
     return result
