eb9699e7b2e57eed6fdbd6b2a28c329c883153e8
Adrien Legros <alegros@redhat.com>
2023-09-22 14:43:57 +0200

    verbose

--- inference_service/api.py
+++ inference_service/api.py
@@ -52,8 +52,12 @@ async def predictions(file: UploadFile = File(...)):
     result = Detections(detections=[])
     for raw_detection in raw_detections:
         # Boxes are returned in xMax,xMin,yMax,yMin coordinates on the 640x640 image
+        print(raw_detection)
         box = Box(xMax=raw_detection[2]/640, xMin=raw_detection[0]/640, yMax=raw_detection[3]/640, yMin=raw_detection[1]/640)
+        print(box)
         class_number = int(raw_detection[5])
+        print(class_number)
+        print(classes)
         detection = Detection(box=box, 
             cValue=COUPON_VALUE[class_number], class_=classes[class_number], label=classes[class_number].capitalize(), score=raw_detection[4])
         print(detection)
